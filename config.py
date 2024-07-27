10141abd66469e1451211e50d0c13696f41b77e3
ram6262 <rashpalnishad3@gmail.com>
2024-07-27 18:10:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7382924504:AAFPdn7Fmu6cXs_uhEOJZ1bmeR-t_nwFZ94")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Jadu")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1819374004")
 
 
 
