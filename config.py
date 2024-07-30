b62db3251864aceb10a21240232269e0fe73180f
ram6262a <ddos12262@gmail.com>
2024-07-31 01:03:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("6801992281:AAEr_GaiLnITui-YCUW_MOVtqR_e_aKXi-s")
+BOT_TOKEN=("7323296220:AAEcF0_gWjAYrDxL49BtNWFXm3KETQNkjsU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("ggggram_bot")
+OWNER_USERNAME=("bgmi_public_bot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1306000780")
